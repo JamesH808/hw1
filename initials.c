cf71500e1cad450ce17236f4aed24a9dd7d02f9b
jaholt <jaholt@students.chapman.edu>
2023-11-27 09:54:24 -0800

    adding to remote github

---
+++ initials.c
@@ -0,0 +1,40 @@
+/* initials.c: first homework assignment */
+
+/* author: James Holt                    */
+
+/* modifications:                        */
+/* 2/2/2023 EA initial version           */
+
+#include <stdio.h>
+
+/* Main Function*/
+
+int main(void)
+{
+        printf("\n");
+        printf(" JJ         J\n");
+	printf("J           J\n");
+	printf(" JJ         J\n");
+	printf("   JJJJJJJJJJ\n");
+	printf("            J\n");
+	printf("            J\n");
+	printf("            J\n");
+        printf("\n");
+
+	printf("KKKKKKKKKKKKK\n");
+	printf("      K      \n");
+	printf("    KK KK    \n");
+	printf("  KK     KK  \n");
+	printf(" K         K \n");
+	printf("\n");
+      
+	printf("HHHHHHHHHHHHH\n");
+	printf("      H      \n");
+	printf("      H      \n");
+	printf("      H      \n");
+	printf("HHHHHHHHHHHHH\n");
+	printf("\n");
+
+
+        return 0;
+}
